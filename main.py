06f07a7f2e9162cec5621380c5aaa814674f4451
UsBPoRRtHuN <peti7811@gmail.com>
2021-10-21 11:48:43 +0200

    removed dropdown menu, added options with popup. no textboxes yet

--- main.py
+++ main.py
@@ -1,32 +1,12 @@
+from kivy.config import Config
 from kivy.app import App
-from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+Config.set('graphics', 'resizable', True)
 
-
+class Thesis(GridLayout):
+    pass
+class ThesisApp(App):
+    def build(self):
+        return Thesis()
 if __name__ == '__main__':
-    from kivy.uix.dropdown import DropDown
-    from kivy.uix.button import Button
-    from kivy.base import runTouchApp
-    dropdown = DropDown()
-    names = ["options", "exit"]
-    for index in range(2):
-
-        btn = Button(text=names[index], size_hint_y=None, height=20)
-        btn.bind(on_release=lambda btn: dropdown.select(btn.text))
-
-        # then add the button inside the dropdown
-        dropdown.add_widget(btn)
-
-    # create a big main button
-    mainbutton = Button(text='Hello', size_hint=(None, None))
-
-    # show the dropdown menu when the main button is released
-    # note: all the bind() calls pass the instance of the caller (here, the
-    # mainbutton instance) as the first argument of the callback (here,
-    # dropdown.open.).
-    mainbutton.bind(on_release=dropdown.open)
-
-    # one last thing, listen for the selection in the dropdown list and
-    # assign the data to the button text.
-    dropdown.bind(on_select=lambda instance, x: setattr(mainbutton, 'text', x))
-
-    runTouchApp(mainbutton)
+    ThesisApp().run()
