5e0642a26f57f7570dc49bd738d9458a965ed724
UsBPoRRtHuN <peti7811@gmail.com>
2021-10-21 08:39:30 +0200

    testing

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 if __name__ == '__main__':
-    print_hi('jóska')
+    print_hi('lorem ipsum dolor sit amet vagy mi')
 
