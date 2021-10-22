a59965f973b73a0ac74017bdbbfcfc7c0769800c
UsBPoRRtHuN <peti7811@gmail.com>
2021-10-22 13:52:11 +0200

    Few little changes, some cleanup

--- UI/window.py
+++ UI/window.py
@@ -2,21 +2,25 @@ from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.widget import Widget
 from kivy.lang import Builder
-
+#TODO Basic textet törölje, de az utána jövő adatokat ne
+#TODO vizsgálni a paramétereket, pl. 1000 szintes ne legyen valami
+#TODO érintéses propertyt kikapcsolni
 
 class Window(GridLayout):
     pass
 
 
 class WindowApp(App):
+
     def build(self):
         return Window()
 
     def process(self):
-        text = self.root.ids.BaseAreaTextInput.text
-        text2 = self.root.ids.LevelsTextInput.text
+        return 0
 
     def show(self):
+        self.root.ids.BaseAreaTextInput.text = "Enter the area..."
+        self.root.ids.LevelsTextInput.text = "No of max levels..."
         self.root.ids.BaseAreaTextInput.opacity = 1
         self.root.ids.LevelsTextInput.opacity = 1
 
@@ -24,8 +28,9 @@ class WindowApp(App):
         self.root.ids.BaseAreaTextInput.opacity = 0
         self.root.ids.LevelsTextInput.opacity = 0
 
-    def clearText(self):
-        self.root.ids.BaseAreaTextInput.text = ""
+    def print(self):
+        print (self.root.ids.BaseAreaTextInput.text)
+        print (self.root.ids.LevelsTextInput.text)
 
 
 def create_window():
--- main.py
+++ main.py
@@ -4,3 +4,4 @@ import UI.window
 
 if __name__ == '__main__':
     UI = UI.window.create_window()
+
