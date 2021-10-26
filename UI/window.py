c609e15872650077e2f650b5de7b983e9025ea74
UsBPoRRtHuN <peti7811@gmail.com>
2021-10-26 14:02:51 +0200

    The basic layout is done. Buttons, labels added. No functionalities

--- UI/window.py
+++ UI/window.py
@@ -1,12 +1,8 @@
 from kivy.app import App
-from kivy.uix.gridlayout import GridLayout
 from kivy.uix.widget import Widget
-from kivy.lang import Builder
-#TODO Basic textet törölje, de az utána jövő adatokat ne
-#TODO vizsgálni a paramétereket, pl. 1000 szintes ne legyen valami
-#TODO érintéses propertyt kikapcsolni
 
-class Window(GridLayout):
+
+class Window(Widget):
     pass
 
 
@@ -18,20 +14,26 @@ class WindowApp(App):
     def process(self):
         return 0
 
-    def show(self):
-        self.root.ids.BaseAreaTextInput.text = "Enter the area..."
-        self.root.ids.LevelsTextInput.text = "No of max levels..."
-        self.root.ids.BaseAreaTextInput.opacity = 1
-        self.root.ids.LevelsTextInput.opacity = 1
+    def showArea(self):
+        self.root.ids.Area.opacity = 1
+        self.root.ids.Area.disabled = False
 
-    def hide(self):
-        self.root.ids.BaseAreaTextInput.opacity = 0
-        self.root.ids.LevelsTextInput.opacity = 0
+    def showLevels(self):
+        self.root.ids.Levels.opacity = 1
+        self.root.ids.Levels.disabled = False
 
-    def print(self):
-        print (self.root.ids.BaseAreaTextInput.text)
-        print (self.root.ids.LevelsTextInput.text)
+    def hideArea(self):
+        self.root.ids.Area.opacity = 0
+        self.root.ids.Area.disabled = True
 
+    def hideLevels(self):
+        self.root.ids.Levels.opacity = 0
+        self.root.ids.Levels.disabled = True
+
+    def saveValues(self):
+        area = self.root.ids.Area.text
+        levels = self.root.ids.Levels.text
+        print(area, levels)
 
 def create_window():
     WindowApp().run()
