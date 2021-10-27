cfb2a2e68fd4fbf42ed1a4adaa378dc81ded2949
UsBPoRRtHuN <peti7811@gmail.com>
2021-10-27 11:59:52 +0200

    added basic button functionality (options, exit)

--- UI/window.py
+++ UI/window.py
@@ -11,5 +11,19 @@ class WindowApp(App):
     def build(self):
         return Window()
 
+    def optionstogglebutton(self, togglebutton):
+
+        if (togglebutton.state == "down"):
+            self.root.ids.AreaInput.disabled = 0
+            self.root.ids.LevelsInput.disabled = 0
+            self.root.ids.AreaInput.opacity = 1
+            self.root.ids.LevelsInput.opacity = 1
+        else:
+            self.root.ids.AreaInput.disabled = 1
+            self.root.ids.LevelsInput.disabled = 1
+            self.root.ids.AreaInput.opacity = 0
+            self.root.ids.LevelsInput.opacity = 0
+
+
 def create_window():
     WindowApp().run()
