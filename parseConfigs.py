576c79a945c1a7bf55b380aad04a7d66f9396a97
UsBPoRRtHuN <peti7811@gmail.com>
2021-11-04 08:11:19 +0100

    reading from json

--- parseConfigs.py
+++ parseConfigs.py
@@ -3,4 +3,4 @@ import json
 def parseJson():
     with open('C:\\Users\\Takacs.Peter\\Desktop\\Thesis\\Data\\GroupConfigs.json', 'r') as myfile:
         data = myfile.read()
-        obj = json.loads(data)
+        group = json.loads(data)
