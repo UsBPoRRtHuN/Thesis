6c8089ce148a7bbe665dd360236aeff6752ad686
UsBPoRRtHuN <peti7811@gmail.com>
2021-11-17 19:50:09 +0100

    BaseGroup Top,left,down,right borders added

--- Model/BaseGroup.py
+++ Model/BaseGroup.py
@@ -3,8 +3,9 @@ import Model.BaseUnit
 #default érték
 
 insideTheBuilding = True
-groupSize = 16
-
+width = 4
+height = 4
+groupSize = width * height
 
 class BaseGroup():
     size = groupSize * Model.BaseUnit.BaseUnit.size
--- generate.py
+++ generate.py
@@ -30,24 +30,40 @@ class Generate:
 
     def examineCompatibility(self):
         borders = []
+        finalBorders = {}
         self.getBorders(self, borders)
+        self.createBorders(self, borders, finalBorders)
+        print(finalBorders)
 
     def getBorders(self, borders):
-        item = []
         for i in range(len(self.baseUnitList)):
+            item = []
             for j in range(len(self.baseUnitList[i])):
                 if j == 0 or j == (len(self.baseUnitList[i]) - 1):
                     item.append(self.baseUnitList[i][j])
                 else:
+                    temp = []
                     for k in range(len(self.baseUnitList[i][j])):
                         if k == 0 or k == (len(self.baseUnitList[i][j]) - 1):
-                            item.append(self.baseUnitList[i][j][k])
-        print(type(item[0]))
+                            temp.append(self.baseUnitList[i][j][k])
+                    item.append(temp)
+            borders.append(item)
+
+    def createBorders(self, borders, finalBorders):
+        for i in range(len(borders)):
+            left = []
+            right = []
+
+            top = [borders[i][0]]  # Első elem, ez mindig a teteje lesz a BaseGroupnak
+            down = [borders[i][-1]]  # BaseGroup magasságát felhasználva megkapom az utolsó elemét
+            for j in range(len(borders[i])):
+                left.append(borders[i][j][0])  # bal széle
+                right.append(borders[i][j][-1])  # jobb széle
+            groupBorder = [top, right, down, left]
+            finalBorders[i] = groupBorder
 
     def calculate(self):
         self.noOfBaseGroups = (int(self.area) / (int(BaseUnit.size) * int(BaseGroup.groupSize))) / int(self.levels)
-        print(self.noOfBaseGroups)
-        print(self.levels)
 
     def loadConfigs(self):
         self.baseGroupList = parseConfigs.parseBaseGroupJson()
