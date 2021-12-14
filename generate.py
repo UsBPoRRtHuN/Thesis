e0798a785c4793e72bd9f36f30c52f9c8dacace6
UsBPoRRtHuN <peti7811@gmail.com>
2021-12-15 00:46:54 +0100

    the final release for today: removed border inspection, the filtering might be too sensitive now, but overall looks good

--- generate.py
+++ generate.py
@@ -25,7 +25,6 @@ class Generate:
     def init(self):
         self.loadConfigs(self)
         self.calculate(self)
-        self.examineCompatibility(self)
         self.generateLayouts(self)
 
     def getLayout(self, num):
@@ -51,73 +50,6 @@ class Generate:
                         self.layoutList.append(lay)
 
         self.noOfLayouts = len(self.layoutList)
-
-    def examineCompatibility(self):
-        borders = []
-        finalBorders = {}
-        self.matchingBorders = {}
-        self.getBorders(self, borders)
-        self.createBorders(self, borders, finalBorders)
-        self.matchingBorders = self.compareBorders(self, finalBorders)
-
-    def compareBorders(self, finalBorders):
-        temp = list(finalBorders)
-        matchingBorders = {}
-        i = -1
-        for key in finalBorders:
-            try:
-                res = temp[temp.index(key) + 1]
-            except (ValueError, IndexError):
-                res = None
-            for pos in range(len(finalBorders[key])):
-                for border in (finalBorders[key][pos]):
-                    try:
-                        for res in range(len((finalBorders))):
-                            if int(key) < int(res):  # önmagánál kisebbet már ne vizsgáljon
-                                for comparingPos in range(len(finalBorders[res])):
-                                    for comparingBorder in (finalBorders[res][comparingPos]):
-                                        if finalBorders[key][pos][border] == finalBorders[res][comparingPos][
-                                            comparingBorder] and finalBorders[res] is not finalBorders[
-                                            key]:  # önmagát ne vizsgálja
-                                            i += 1
-                                            # print("A " + str(key) + ". groupunit" + str(finalBorders[key][pos]) + " oldala megegyezik a " + str(res) + str(finalBorders[res][comparingPos]) + "oldalával")
-                                            matchingBorders[i] = [str(key), finalBorders[key][pos]], [str(res),
-                                                                                                      finalBorders[res][
-                                                                                                          comparingPos]]
-                    except (ValueError, IndexError, KeyError):
-                        pass
-        return matchingBorders
-
-    def getBorders(self, borders):
-        for i in range(len(self.baseUnitList)):
-            item = []
-            for j in range(len(self.baseUnitList[i])):
-                if j == 0 or j == (len(self.baseUnitList[i]) - 1):
-                    item.append(self.baseUnitList[i][j])
-                else:
-                    temp = []
-                    for k in range(len(self.baseUnitList[i][j])):
-                        if k == 0 or k == (len(self.baseUnitList[i][j]) - 1):
-                            temp.append(self.baseUnitList[i][j][k])
-                    item.append(temp)
-            borders.append(item)
-
-    def createBorders(self, borders, finalBorders):
-        for i in range(len(borders)):
-            leftList = []
-            rightList = []
-            left = {}
-            right = {}
-            top = {'top': [borders[i][0]]}
-            down = {'down': [borders[i][-1]]}
-            # Első elem, ez mindig a teteje lesz a BaseGroupnak
-            # BaseGroup magasságát felhasználva megkapom az utolsó elemét
-            for j in range(len(borders[i])):
-                leftList.append(borders[i][j][0])  # bal széle
-                rightList.append(borders[i][j][-1])  # jobb széle
-            left["left"] = leftList
-            right["right"] = rightList
-            finalBorders[i] = top, right, down, left
 
     def checkRatio(self, allVariations):
         allVariationsFiltered = []
@@ -261,7 +193,6 @@ class Generate:
                 counterList = []
         for items in counterListList:
             transposedVariation = []
-            print(items)
             for index in items:
                 m = variations[index]
                 rez = [[m[j][i] for j in range(len(m))] for i in range(len(m[0]))]
